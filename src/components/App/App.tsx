32eb0eb5551af1d284e631ec87bdfd6a446ea168
piotr04j <darricck04@gmail.com>
2021-05-09 11:49:09 +0200

    used improved useDispatch

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import logo from '../../logo.svg';
 import './App.css';
+import { fetchPosts } from '../../store/slice/posts/postsSlice';
+import {useAppDispatch} from '../../store/hook/hook';
 
 function App() {
-  return (
+   const dispatch = useAppDispatch()
+
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <div>
+          <button onClick={() => dispatch(fetchPosts())}>
+              Get Posts!
+          </button>
+      </div>
     </div>
   );
 }
