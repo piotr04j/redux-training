74e1b4e168c8729d32b4b52623923df5d2296c99
piotr04j <darricck04@gmail.com>
2021-05-17 19:03:51 +0200

    prepera app to show posts list

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import logo from '../../logo.svg';
 import './App.css';
-import { fetchPosts } from '../../store/slice/posts/postsSlice';
+import {fetchPosts, selectAllPostsAdapter} from '../../store/slice/posts/postsSlice';
 import {useAppDispatch} from '../../store/hook/hook';
+import { useSelector } from 'react-redux';
 
 function App() {
-   const dispatch = useAppDispatch()
+    const dispatch = useAppDispatch()
+    const posts = useSelector(selectAllPostsAdapter.selectAll)
 
     return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <div>
-          <button onClick={() => dispatch(fetchPosts())}>
-              Get Posts!
-          </button>
-      </div>
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <div>
+            <button onClick={() => dispatch(fetchPosts())}>
+                Get Posts!
+            </button>
+        </div>
+        <ul>
+            {
+                posts && posts.map( item => {
+                    return <li key={item.id}>{item.title}</li>
+                })
+            }
+        </ul>
     </div>
   );
 }
