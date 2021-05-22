132c58d64cacaa4f14bbd054f1d48965a484bef6
piotr04j <darricck04@gmail.com>
2021-05-22 15:18:09 +0200

    removed redundant provider from modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,16 +1,14 @@
 import { createPortal } from 'react-dom';
 import './Modal.css'
-import {Provider} from 'react-redux';
-import store from '../../store/store';
 import ModalContent from './ModalContent';
 
 const Modal = () => {
     const modalRoot = document.getElementById('modal');
     if(modalRoot) {
         return createPortal(
-            <Provider store={store}>
+            <>
                 <ModalContent />
-            </Provider>, modalRoot);
+            </>, modalRoot);
     }
 
     return null
